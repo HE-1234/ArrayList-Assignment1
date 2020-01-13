791ce0a8d985114284fe7982bd4218424acc2318
Eric Huang <56049709+HE-1234@users.noreply.github.com>
2020-01-13 13:30:51 -0800

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Q2 {
+    
+    public static void removeSmallest(ArrayList<Integer> nums) {
+        int small = nums.get(0);
+        int seq = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums.get(i)>small){
+            }else if (nums.get(i)<small){
+                seq = i;
+            }
+        }
+        
+        nums.remove(seq);
+    }
+  
+    public static void main (String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            nums.add( (int) (Math.random()*100) );
+        }
+        
+        System.out.println("Our list before: " + nums);
+        removeSmallest(nums); 
+        System.out.println("Our list after:  " + nums);
+    }
+    
+    
+}
